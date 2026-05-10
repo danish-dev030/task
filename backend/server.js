754869ff65aa9251b2324706e27043590bc9e8dd
danish-dev030 <danishdev030@gmail.com>
2026-05-10 19:44:52 +0500

    - added the s3 url for cors allow

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://task-theta-drab-11.vercel.app'],
+    origin: ['http://localhost:5173', 'https://task-theta-drab-11.vercel.app', 'http://pixora-assign.s3-website-us-east-1.amazonaws.com'],
     credentials: true
   })
 );
